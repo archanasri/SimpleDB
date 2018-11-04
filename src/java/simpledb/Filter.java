8d1955d175c628cdaf6791fba9edcd6569352bae
archanasris <archanasri.subramanian@utah.edu>
2018-11-04 16:32:09 -0700

    Revert "Changed Filter.java"
    This reverts commit e06236bc7c7c5d7e65234f786022360ee844c402.

--- src/java/simpledb/Filter.java
+++ src/java/simpledb/Filter.java
@@ -8,8 +8,7 @@ import java.util.*;
 public class Filter extends Operator {
 
     private static final long serialVersionUID = 1L;
-    private Predicate mPredicate;
-    private DbIterator mChild;
+
     /**
      * Constructor accepts a predicate to apply and a child operator to read
      * tuples to filter from.
@@ -21,36 +20,29 @@ public class Filter extends Operator {
      */
     public Filter(Predicate p, DbIterator child) {
         // some code goes here
-    	mPredicate = p;
-    	mChild = child;
     }
 
     public Predicate getPredicate() {
         // some code goes here
-    	return mPredicate;
+        return null;
     }
 
     public TupleDesc getTupleDesc() {
         // some code goes here
-    	return mChild.getTupleDesc();
+        return null;
     }
 
     public void open() throws DbException, NoSuchElementException,
             TransactionAbortedException {
         // some code goes here
-    	mChild.open();
-    	super.open();
     }
 
     public void close() {
         // some code goes here
-    	super.close();
-    	mChild.close();
     }
 
     public void rewind() throws DbException, TransactionAbortedException {
         // some code goes here
-    	mChild.rewind();
     }
 
     /**
@@ -65,27 +57,18 @@ public class Filter extends Operator {
     protected Tuple fetchNext() throws NoSuchElementException,
             TransactionAbortedException, DbException {
         // some code goes here
-    	while(mChild.hasNext() != false) {
-    		Tuple t = mChild.next();
-    		if (mPredicate.filter(t) == true) {
-    			return t;
-    		}
-    	}
-    	return null;
+        return null;
     }
 
     @Override
     public DbIterator[] getChildren() {
         // some code goes here
-    	DbIterator[] result = new DbIterator[1];
-    	result[0] = mChild;
-        return result;
+        return null;
     }
 
     @Override
     public void setChildren(DbIterator[] children) {
         // some code goes here
-    	mChild = children[0];
     }
 
 }
