74e539f9581ce281e0fd49fe4860d8af84d23a66
Chanchal Kariwala <chanchalkariwala@gmail.com>
2018-12-07 14:21:52 -0700

    Project 5

--- src/java/simpledb/BufferPool.java
+++ src/java/simpledb/BufferPool.java
@@ -1,11 +1,12 @@
 package simpledb;
 
 import java.io.IOException;
-import java.util.Deque;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * BufferPool manages the reading and writing of pages into memory from
@@ -33,8 +34,10 @@ public class BufferPool {
     
     private Map<PageId, Page> mmapPages;
     
+    private ConcurrencyControl mCControl;
+    
     //Using FIFO approach for Eviction Policy
-    private Deque<PageId> mPidQueue;
+    //private Deque<PageId> mPidQueue;
     
     /**
      * Creates a BufferPool that caches up to numPages pages.
@@ -44,7 +47,8 @@ public class BufferPool {
     public BufferPool(int numPages) {
     	miNumPages = numPages;
     	mmapPages = new HashMap<>();
-    	mPidQueue = new LinkedList<>();
+    	//mPidQueue = new LinkedList<>();
+    	mCControl = new ConcurrencyControl();
     }
     
     public static int getPageSize() {
@@ -79,6 +83,14 @@ public class BufferPool {
     public  Page getPage(TransactionId tid, PageId pid, Permissions perm)
         throws TransactionAbortedException, DbException {
     	
+    	LockType lType;
+        if(perm == Permissions.READ_ONLY)
+            lType = LockType.Slock;
+        else
+            lType = LockType.Xlock;
+
+        mCControl.acquireLock(tid, pid, lType);
+    	
     	if(mmapPages.containsKey(pid))
     		return mmapPages.get(pid);
     	
@@ -92,9 +104,10 @@ public class BufferPool {
     	//There is space. Get Page and add to map.
     	Page lPage = Database.getCatalog().getDatabaseFile(pid.getTableId()).readPage(pid);
     	mmapPages.put(pid, lPage);
-    	mPidQueue.addLast(pid);
+    	//mPidQueue.addLast(pid);
     	
         return lPage;
+    	    	
     }
 
     /**
@@ -107,8 +120,8 @@ public class BufferPool {
      * @param pid the ID of the page to unlock
      */
     public  void releasePage(TransactionId tid, PageId pid) {
-        // some code goes here
-        // not necessary for lab1|lab2|lab3|lab4
+
+    	mCControl.releaseLock(tid, pid);
     }
 
     /**
@@ -117,15 +130,14 @@ public class BufferPool {
      * @param tid the ID of the transaction requesting the unlock
      */
     public void transactionComplete(TransactionId tid) throws IOException {
-        // some code goes here
-        // not necessary for lab1|lab2|lab3|lab4
+
+    	transactionComplete(tid, true);
     }
 
     /** Return true if the specified transaction has a lock on the specified page */
     public boolean holdsLock(TransactionId tid, PageId p) {
-        // some code goes here
-        // not necessary for lab1|lab2|lab3|lab4
-        return false;
+        
+    	return mCControl.holdsLock(tid, p);
     }
 
     /**
@@ -137,8 +149,22 @@ public class BufferPool {
      */
     public void transactionComplete(TransactionId tid, boolean commit)
         throws IOException {
-        // some code goes here
-        // not necessary for lab1|lab2|lab3|lab4
+        
+    	List<PageId> lLockedPages = mCControl.mTIdPageMap.get(tid);
+        if(lLockedPages!=null){
+            for(PageId pageId: lLockedPages)
+            {
+                if(commit)
+                {
+                    flushPage(pageId);
+                } 
+                else if(mmapPages.get(pageId) != null && mmapPages.get(pageId).isDirty() != null)
+                {
+                    discardPage(pageId);
+                }
+            }
+        }
+        mCControl.releaseLock(tid);
     }
 
     /**
@@ -221,9 +247,9 @@ public class BufferPool {
         are removed from the cache so they can be reused safely
     */
     public synchronized void discardPage(PageId pid) {
-        // some code goes here
-        // not necessary for lab1
-    }
+
+    	mmapPages.remove(pid);
+    }	
 
     /**
      * Flushes a certain page to disk
@@ -232,15 +258,30 @@ public class BufferPool {
     private synchronized  void flushPage(PageId pid) throws IOException {
 
     	// flushPage should write any dirty page to disk and mark it as not dirty, while leaving it in the BufferPool
-    	Database.getCatalog().getDatabaseFile(pid.getTableId()).writePage(mmapPages.get(pid));
+		//Database.getCatalog().getDatabaseFile(pid.getTableId()).writePage(mmapPages.get(pid));
+    	
+    	if(mmapPages.containsKey(pid))
+    	{
+            Page lPage = mmapPages.get(pid);
+            if(lPage.isDirty()!= null)
+            {
+                Database.getCatalog().getDatabaseFile(pid.getTableId()).writePage(lPage);
+                lPage.markDirty(false, null);
+            }
+        }
     	
     }
 
     /** Write all pages of the specified transaction to disk.
      */
     public synchronized  void flushPages(TransactionId tid) throws IOException {
-        // some code goes here
-        // not necessary for lab1|lab2|lab3|lab4
+
+    	if(mCControl.mTIdPageMap.containsKey(tid))
+    	{
+            ArrayList<PageId> llistPages = mCControl.mTIdPageMap.get(tid);
+            for (PageId lPage : llistPages)
+                flushPage(lPage);
+        }
     }
 
     /**
@@ -252,22 +293,189 @@ public class BufferPool {
     	// The only method which should remove page from the buffer pool is evictPage, 
     	// which should call flushPage on any dirty page it evicts.
     	
-    	PageId pid = mPidQueue.pollFirst();
+//    	PageId pid = mPidQueue.pollFirst();
+//    	
+//    	if(pid != null)
+//    	{
+//    		try 
+//    		{
+//				flushPage(pid);
+//				mmapPages.remove(pid);
+//			} 
+//    		catch (IOException e) 
+//    		{
+//				throw new DbException("Error while writing page to disk.");
+//			}
+//    		
+//    	}
     	
-    	if(pid != null)
+    	for (Map.Entry<PageId, Page> lEntry : mmapPages.entrySet()) 
     	{
-    		try 
-    		{
-				flushPage(pid);
-				mmapPages.remove(pid);
-			} 
-    		catch (IOException e) 
-    		{
-				throw new DbException("Error while writing page to disk.");
-			}
-    		
-    	}
+            PageId lPId = lEntry.getKey();
+            Page lPage = lEntry.getValue();
+            if (lPage.isDirty() == null) 
+            {                
+                discardPage(lPId);
+                return;
+            }
+        }
+        throw new DbException("BufferPool: evictPage: all pages are marked as dirty");
     	
     }
 
+}
+
+enum LockType{Slock, Xlock}
+
+class LockData
+{
+    LockType mLockType;
+    ArrayList<TransactionId> mTransactions;
+
+    LockData(LockType pLockType, ArrayList<TransactionId> pTId)
+    {
+        this.mLockType = pLockType;
+        this.mTransactions = pTId;
+    }
+}
+
+class ConcurrencyControl
+{
+    ConcurrentHashMap<TransactionId, ArrayList<PageId>> mTIdPageMap = new ConcurrentHashMap<TransactionId, ArrayList<PageId>>();
+    ConcurrentHashMap<PageId, LockData> mPageLockMap = new ConcurrentHashMap<PageId, LockData>();
+
+    private synchronized void block(long pStart, long pTimeout) throws TransactionAbortedException 
+    {
+        if (System.currentTimeMillis() - pStart > pTimeout) 
+        {
+            throw new TransactionAbortedException();
+        }
+
+        try 
+        {
+            wait(pTimeout);
+            if (System.currentTimeMillis() - pStart > pTimeout) 
+            {
+                throw new TransactionAbortedException();
+            }
+        } catch (InterruptedException e) 
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized void acquireLock(TransactionId pTId, PageId pPId, LockType pType) throws TransactionAbortedException
+    {
+        long lStart = System.currentTimeMillis();
+        Random lRandom = new Random();
+        long lTimeout = lRandom.nextInt(2000);
+        while(true) 
+        {
+        	//Nobody has a lock on the page
+        	if (mPageLockMap.containsKey(pPId) == false)
+        	{
+	        	updateTIdPageMap(pTId, pPId);
+	            ArrayList<TransactionId> lListTId = new ArrayList<TransactionId>();
+	            lListTId.add(pTId);
+	            mPageLockMap.put(pPId, new LockData(pType, lListTId));
+	            return;
+        	}
+    		
+        	//Somebody has an Exclusive lock on the page
+        	if (mPageLockMap.get(pPId).mLockType == LockType.Xlock)
+        	{
+        		if (mPageLockMap.get(pPId).mTransactions.get(0) == pTId) 
+            	{
+            		return;
+            	}
+        		
+            	block(lStart, lTimeout);
+            	continue;
+        	}
+        	
+        	//Somebody has a Shared lock on the page
+        	
+        	if (pType == LockType.Slock) 
+            {
+        		//Current Transaction is requesting Shared Lock too
+                if (mPageLockMap.get(pPId).mTransactions.contains(pTId) == false) 
+                {
+                    mPageLockMap.get(pPId).mTransactions.add(pTId);
+                }
+                updateTIdPageMap(pTId, pPId);
+                return;
+            } 
+            else 
+            {
+            	//Current Transaction is requesting Exclusive Lock
+                if(( mPageLockMap.get(pPId).mTransactions.size() == 1 ) 
+                		&& ( mPageLockMap.get(pPId).mTransactions.get(0) == pTId ) 
+                		&& ( mTIdPageMap.containsKey(pTId) )
+                		&& ( mTIdPageMap.get(pTId).contains(pPId) )) 
+                {
+                    mPageLockMap.get(pPId).mLockType = LockType.Xlock;
+                    return;
+                } 
+                block(lStart, lTimeout);
+            }
+        }
+    }
+
+    private synchronized void updateTIdPageMap(TransactionId pTId, PageId pPId)
+    {
+        if(mTIdPageMap.containsKey(pTId))
+        {
+            if(mTIdPageMap.get(pTId).contains(pPId) == false)
+            	mTIdPageMap.get(pTId).add(pPId);
+            return;
+        }
+        
+        ArrayList<PageId> lPageIdList = new ArrayList<PageId>();
+        lPageIdList.add(pPId);
+        mTIdPageMap.put(pTId, lPageIdList);
+    }
+
+    public synchronized void releaseLock(TransactionId pTId, PageId pPId) 
+    {
+        if (mTIdPageMap.containsKey(pTId)) 
+        {
+            mTIdPageMap.get(pTId).remove(pPId);
+            if (mTIdPageMap.get(pTId).size() == 0) 
+            {
+                mTIdPageMap.remove(pTId);
+            }
+        }
+        if (mPageLockMap.containsKey(pPId)) 
+        {
+            mPageLockMap.get(pPId).mTransactions.remove(pTId);
+            if (mPageLockMap.get(pPId).mTransactions.size() == 0) 
+            	 mPageLockMap.remove(pPId);
+            else 
+            	notifyAll();
+        }
+    }
+
+    public synchronized void releaseLock(TransactionId pTId)
+    {
+        if(mTIdPageMap.containsKey(pTId))
+        {
+            PageId[] pages = new PageId[mTIdPageMap.get(pTId).size()];
+            PageId[] pagesToRelease = mTIdPageMap.get(pTId).toArray(pages);
+            for(PageId pid: pagesToRelease)
+            {
+                releaseLock(pTId, pid);
+            }
+        }
+    }
+
+    public synchronized boolean holdsLock(TransactionId pTId, PageId pid) 
+    {
+        if (mTIdPageMap.containsKey(pTId)) 
+        {
+            if (mTIdPageMap.get(pTId).contains(pid)) {
+                return true;
+            }
+        }
+        return false;
+    }   
 }
